eede3c1913e1a86ebf6c97b2767ac5b3bb65b8fe
Diana <dianalyvytska@gmail.com>
2023-02-13 13:25:16 +0200

    configured eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,11 +11,14 @@ module.exports = {
   ],
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
+    project: ['tsconfig.json']
   },
   plugins: [
     'react'
   ],
   rules: {
+    semi: ['error', 'always'],
+    '@typescript-eslint/semi': ['error', 'always']
   }
-}
+};
