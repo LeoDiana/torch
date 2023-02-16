d7fb588adabb8e04e09aee4ccab55886074f3f00
Hrinchenko Marharyta <mhrinchenko@exadel.com>
2023-02-17 00:53:03 +0200

    create card with styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Card from './components/Card';
 
-function App (): JSX.Element {
+const App = (): JSX.Element => {
   return (
-    <div className='text-3xl font-bold underline'>HELLO BLIAT WORLD</div>
+    <Card />
   );
-}
+};
 
 export default App;
